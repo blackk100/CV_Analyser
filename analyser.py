5d483bd6997d713ec9ace26ee2191a4d5955ebd4
blackk100 <aryaman890@outlook.com>
2018-04-23 17:17:04 +0530

    Color Conversion + Better Documentation + Cleanup

--- analyser.py
+++ analyser.py
@@ -8,76 +8,105 @@ Made with: PyCharm Community and pipenv
 """
 import numpy
 import cv2
-log, err = [], ""
 
 
-def read():
-	""" Reads and returns an image (includes user interactions) """
-	log.append("read() Started")
+def read() -> int:
+	"""
+	Reads and returns an image (includes user interactions)
+
+	:return: NumPy int8 array (OpenCV Image Representation)
+	:rtype: int
+	"""
 	from pathlib import Path  # Used for checking if the file exists
-	global err
 	while True:
 		print("Enter relative path to the image to be read (including the filename with extension):")
 		path = input()
 		file = Path(path)
-		log.append("Path input: " + path)
 		if file.is_file():
-			log.append("File-path Verified")
 			break
 		else:
-			err = "ERROR: Incorrect file-path!!"
-			print(err)
-			log.append(err)
+			print("ERROR: Incorrect file-path!!")
 			continue
 	while True:
 		print("Enter the image read mode:")
 		print("\t0 - Colored image")
 		print("\t1 - Black and White image")
 		mode = int(input())
-		log.append("Mode input: " + str(mode) + "")
 		if mode in [0, 1]:
 			mode = cv2.IMREAD_COLOR if 0 else cv2.IMREAD_GRAYSCALE
-			log.append("Image Read Mode Verified and Assigned Assigned")
 			break
 		else:
-			err = "ERROR: Value out of range!!"
-			print(err)
-			log.append(err)
+			print("ERROR: Value out of range!!")
 			continue
 	image = cv2.imread(path, mode)
 	return image
 
 
-def denoise(image, mode=1, quality=0):
+def denoise(image, mode=1, quality=0) -> int:
 	"""
-	Removes noise from the image
-	
-	Parameters:
-		image   -- OpenCV Image Object
-		mode    -- Color mode (default = 1)
-					1 -- Colored image
-					0 -- Gray-scale image
-		quality -- used to specify the de-noising quality (default = 0)
-					-1 -- low (Moderate Noise, High End Image Detail, Very Low Colored Image Distortion)
-					0  -- medium (Low Noise, Moderate End Image Detail, Low Colored Image Distortion)
-					1  -- high (Very Low Noise, Low End Image Detail, Moderate Colored Image Distortion)
+	Removes noise from the input image
+
+	:param image: NumPy int8 array (OpenCV Image Representation)
+	:type image: int
+
+	:param mode: Image color mode (default = 1).
+
+		* 1 -- Colored Image
+		* 0 -- Gray-scale Image
+	:type mode: int
+
+	:param quality: De-noising quality (default = 0)
+
+		* -1 -- low (Moderate Noise, High End Image Detail, Very Low Colored Image Distortion)
+		* 0  -- medium (Low Noise, Moderate End Image Detail, Low Colored Image Distortion)
+		* 1  -- high (Very Low Noise, Low End Image Detail, Moderate Colored Image Distortion)
+	:type quality: int
+
+	:return: NumPy int8 array (OpenCV Image Representation)
+	:rtype: int
 	"""
-	dst = None
-	template_window_size = 7
-	search_window_size = 21
+	t_img = image
+	dst, template_window_size, search_window_size = None, 7, 21
 	if quality == -1:
-		h = 5
-		if mode == 1:
-			h_color = 5
+		h, h_color = 5, 5
 	elif quality == 0:
-		h = 10
-		if mode == 1:
-			h_color = 10
+		h, h_color = 10, 10
 	else:
-		h = 15
-		if mode == 1:
-			h_color = 15
+		h, h_color = 15, 15
 	if mode == 1:
-		cv2.fastNlMeansDenoisingColored(image, dst, h, h_color, template_window_size, search_window_size)
+		cv2.fastNlMeansDenoisingColored(t_img, dst, h, h_color, template_window_size, search_window_size)
 	else:
-		cv2.fastNlMeansDenoising(image, dst, h, template_window_size, search_window_size)
+		cv2.fastNlMeansDenoising(t_img, dst, h, template_window_size, search_window_size)
+	return t_img
+
+
+def change_color(image, mode=0) -> int:
+	"""
+	Changes the image color-space
+
+	:param image: NumPy int8 array (OpenCV Image Representation)
+	:type image: int
+
+	:param mode: Image color conversion mode
+
+		* -1 -- HSV to BGR
+		* 0  -- BGR to Gray-scale
+		* 1  -- BGR to HSV
+		* 2  -- HSV to Gray-scale
+	:type mode: int
+
+	:return: NumPy int8 array (OpenCV Image Representation)
+	:rtype: int
+	"""
+	t_img = image
+	dst, dst_code = None, None
+	if mode == 0:  # BGR to Gray-scale
+		t_img = cv2.cvtColor(t_img, cv2.COLOR_BGR2GRAY, dst, dst_code)
+	elif mode == 1:  # BGR to HSV
+		t_img = cv2.cvtColor(t_img, cv2.COLOR_BGR2HSV, dst, dst_code)
+	elif mode == -1:  # HSV to BGR
+		t_img = cv2.cvtColor(t_img, cv2.COLOR_HSV2BGR, dst, dst_code)
+	elif mode == 2:  # HSV to Gray-scale
+		t_img = change_color(t_img, -1)
+		t_img = change_color(t_img)
+	return t_img
